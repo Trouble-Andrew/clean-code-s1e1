68b8abbde923b61fa28ddf3b27e5a4f03243329e
Trouble-Andrew <satyr663@gmail.com>
2022-05-29 18:31:17 +0300

    refactor: rename tags to match BEM notation according to rule 2.1 (rules 3.1, 3.2, 3.3 depends on this rule) in html-and-css-extended.md

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = 'task-list__item';
+
     label.innerText=taskString;
     label.className='task';
 
@@ -85,11 +87,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("task_edit-mode");
+    //If class of the parent is edit-mode
     if(containsClass){
 
-        //switch to .editmode
+        //switch to edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +100,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle edit-mode on the parent.
+    listItem.classList.toggle("task_edit-mode");
 };
 
 
